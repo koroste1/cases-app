c18a171fe417df9ae53f7912f478adfbe903b240
koroste1 <petro854@gmail.com>
2022-02-03 20:09:34 +0300

    Update data and logic for declension

--- src/AppReducer/AppReducer.js
+++ src/AppReducer/AppReducer.js
@@ -1,15 +1,27 @@
-import {declension, overDecl} from "../data/data";
-import {editableInputTypes} from "@testing-library/user-event/dist/utils";
-
-// let ending = {}
+import {declension, overDecl, secondDecl, thirdDecl} from "../data/data";
 
 export function checkCases(value, word, setNewWord) {
     const findDeclension = declension.find(item => item.name === checkDeclension(word));
-    const ending = findDeclension.word.find(elem=>elem.ending === word.slice(elem.wordSlice));
-
+    console.log(findDeclension)
+    let ending = findDeclension.word.find(elem => elem.ending === word.slice(elem.wordSlice));
     console.log(ending)
-    // console.log(checkDeclension(word));
-    // const ending = declension.find(item => item.word.find(elem => elem.ending === word.slice(-1)));
+    if (!ending) {
+        ending = findDeclension.word.find(elem => elem.ending === '');
+        switch (value) {
+            case 'genitive':
+                return setNewWord(word + ending.cases.genitive);
+            case 'dative':
+                return setNewWord(word + ending.cases.dative);
+            case 'accusative':
+                return setNewWord(word + ending.cases.accusative);
+            case 'instrumental':
+                return setNewWord(word + ending.cases.instrumental);
+            case 'prepositional':
+                return setNewWord('о ' + word + ending.cases.prepositional);
+            default:
+                return;
+        }
+    }
     switch (value) {
         case 'genitive':
             return setNewWord(word.slice(0, ending.wordSlice) + ending.cases.genitive);
@@ -24,37 +36,39 @@ export function checkCases(value, word, setNewWord) {
         default:
             return;
     }
+
 }
 
 export function checkDeclension(word) {
     if (overDecl.find(item => word === item)) {
         return 'over';
+    } else if (thirdDecl.find(item => word === item)) {
+        return 'third';
+    } else if (secondDecl.find(item => word === item)) {
+        return 'second';
     } else if (word.slice(-4) === 'тель' ||
         word.slice(-3) === 'арь' ||
         word.slice(-1) === 'о' ||
         word.slice(-1) === 'е'
-        // declension.map(item => item.word.find(elem => elem.ending === word.slice(-1)))
     ) {
-
         return 'second';
-    } else if (/*word.slice(-2) === 'бь' || 'вь' || 'жь' || 'шь' || 'щь' ||*/
+    } else if (word.slice(-2) === 'жь' ||
+        word.slice(-2) === 'шь' ||
+        word.slice(-2) === 'щь' ||
         word.slice(-3) === 'адь' ||
         word.slice(-3) === 'ать' ||
         word.slice(-3) === 'овь' ||
         word.slice(-4) === 'ость' ||
         word.slice(-4) === 'исть' ||
-        word.slice(-4) === 'есть') {
+        word.slice(-4) === 'есть' ||
+        word.slice(-1) === 'ь' &&
+        secondDecl.find(item => word == item) === undefined) {
         return 'third';
     } else if (word.slice(-1) === 'а' ||
         word.slice(-1) === 'я') {
         return 'first';
-    }
-        //     if (declension.map(item => item.word.find(elem => elem.ending === word.slice(-1)))) {
-        //     const ending = declension.map(item => item.word.find(elem => elem.ending === word.slice(-1)))
-        //     return ending;
-    // }
-
-    else {
+    } else {
+        console.log(word)
         return 'second';
     }
 }
--- src/data/data.js
+++ src/data/data.js
@@ -74,6 +74,17 @@ export const declension = [
                         prepositional: 'е',
                     }
             }, {
+                ending: 'ь',
+                wordSlice: -1,
+                cases:
+                    {
+                        genitive: 'я',
+                        dative: 'ю',
+                        accusative: 'я',
+                        instrumental: 'ем',
+                        prepositional: 'е',
+                    }
+            }, {
                 ending: 'арь',
                 wordSlice: -3,
                 cases:
@@ -109,19 +120,202 @@ export const declension = [
                     }
             },
             {
-                ending: 'ь',
-                wordSlice: -1,
+                ending: '',
+                wordSlice: 0,
                 cases:
                     {
-                        genitive: 'я',
-                        dative: 'ю',
-                        accusative: 'я',
-                        instrumental: 'ем',
+                        genitive: 'а',
+                        dative: 'у',
+                        accusative: '',
+                        instrumental: 'ом',
                         prepositional: 'е',
                     }
             },
         ],
+    }, {
+        name: 'third',
+        word: [
+            {
+                ending: 'ь',
+                wordSlice: -1,
+                cases:
+                    {
+                        genitive: 'и',
+                        dative: 'и',
+                        accusative: 'ь',
+                        instrumental: 'ью',
+                        prepositional: 'и',
+                    }
+            },
+            {
+                ending: 'жь',
+                wordSlice: -2,
+                cases:
+                    {
+                        genitive: 'жи',
+                        dative: 'жи',
+                        accusative: 'жь',
+                        instrumental: 'жью',
+                        prepositional: 'жи',
+                    }
+            },
+            {
+                ending: 'шь',
+                wordSlice: -2,
+                cases:
+                    {
+                        genitive: 'ши',
+                        dative: 'ши',
+                        accusative: 'шь',
+                        instrumental: 'шью',
+                        prepositional: 'ши',
+                    }
+            },
+            {
+                ending: 'чь',
+                wordSlice: -2,
+                cases:
+                    {
+                        genitive: 'чи',
+                        dative: 'чи',
+                        accusative: 'чь',
+                        instrumental: 'чью',
+                        prepositional: 'чи',
+                    }
+            },
+            {
+                ending: 'щь',
+                wordSlice: -2,
+                cases:
+                    {
+                        genitive: 'щи',
+                        dative: 'щи',
+                        accusative: 'щь',
+                        instrumental: 'щью',
+                        prepositional: 'щи',
+                    }
+            },
+            {
+                ending: 'адь',
+                wordSlice: -3,
+                cases:
+                    {
+                        genitive: 'ади',
+                        dative: 'ади',
+                        accusative: 'адь',
+                        instrumental: 'адью',
+                        prepositional: 'ади',
+                    }
+            }, {
+                ending: 'ать',
+                wordSlice: -3,
+                cases:
+                    {
+                        genitive: 'ати',
+                        dative: 'ати',
+                        accusative: 'ать',
+                        instrumental: 'атью',
+                        prepositional: 'ати',
+                    }
+            },
+            {
+                ending: 'овь',
+                wordSlice: -3,
+                cases:
+                    {
+                        genitive: 'ови',
+                        dative: 'ови',
+                        accusative: 'овь',
+                        instrumental: 'овью',
+                        prepositional: 'ови',
+                    }
+            },
+            {
+                ending: 'ость',
+                wordSlice: -4,
+                cases:
+                    {
+                        genitive: 'ости',
+                        dative: 'ости',
+                        accusative: 'ость',
+                        instrumental: 'остю',
+                        prepositional: 'ости',
+                    }
+            },
+            {
+                ending: 'исть',
+                wordSlice: -4,
+                cases:
+                    {
+                        genitive: 'исти',
+                        dative: 'исти',
+                        accusative: 'исть',
+                        instrumental: 'истью',
+                        prepositional: 'исти',
+                    }
+            },
+            {
+                ending: 'есть',
+                wordSlice: -4,
+                cases:
+                    {
+                        genitive: 'ести',
+                        dative: 'ести',
+                        accusative: 'есть',
+                        instrumental: 'естью',
+                        prepositional: 'ести',
+                    }
+            },
+        ],
+    },
+    {
+        name: 'over',
+        word: [
+            {
+                ending: 'мя',
+                wordSlice: -2,
+                cases:
+                    {
+                        genitive: 'мени',
+                        dative: 'мени',
+                        accusative: 'мя',
+                        instrumental: 'менем',
+                        prepositional: 'мени',
+                    }
+            }, {
+                ending: 'ть',
+                wordSlice: -2,
+                cases:
+                    {
+                        genitive: 'ти',
+                        dative: 'ти',
+                        accusative: 'ть',
+                        instrumental: 'тем',
+                        prepositional: 'ти',
+                    }
+            },
+            {
+                ending: 'чь',
+                wordSlice: -2,
+                cases:
+                    {
+                        genitive: 'чи',
+                        dative: 'чи',
+                        accusative: 'чь',
+                        instrumental: 'чью',
+                        prepositional: 'чи',
+                    }
+            }
+        ],
     },
 ]
 
-export const overDecl = ['бремя', 'время', 'вымя', 'знамя', 'имя', 'пламя', 'племя', 'семя', 'стремя', 'темя', 'путь', 'дитя'];
+export const overDecl = ['бремя', 'время', 'вымя', 'знамя', 'имя', 'пламя', 'племя', 'семя', 'стремя', 'темя', 'путь', 'дитя', 'ночь'];
+export const secondDecl = ['тесть', 'ноготь', 'нехристь', 'локоть',
+    'лапоть', 'коготь', 'дёготь', ' ломоть',
+    ' голубь', ' лебедь', ' гусь', 'лось',
+    'князь', 'дождь', 'вождь', 'груздь',
+    'гвоздь','день','камень',
+    'олень', 'конь', 'ячмень' ,'путь' ,'парень',
+    'зверь' ,'шкворень', 'пень' ,'пельмень' ,'тюлень',]
+export const thirdDecl = ['утварь', 'тварь', 'старь', 'пригарь', 'марь', 'изгарь', 'выгарь', 'бездарь', 'гарь', 'постель', 'повитель', 'пастель', 'нетель', 'метель', 'артель']
